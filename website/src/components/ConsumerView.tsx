24ab36798e94b3eae10c545fd065a6fe192ec441
Neel Baronia <nbaronia@gmail.com>
2026-04-08 19:12:58 -0400

    Update pricing teaser copy

--- website/src/components/ConsumerView.tsx
+++ website/src/components/ConsumerView.tsx
@@ -443,7 +443,7 @@ export default function ConsumerView() {
               </span>
             </a>
             <p className="text-sm md:text-base font-medium text-white/85 drop-shadow-[0_2px_16px_rgba(0,0,0,0.42)]">
-              Unlimited access for $9/mo.
+              Unlimited access for $9/mo coming soon...
             </p>
           </motion.div>
           </div>
