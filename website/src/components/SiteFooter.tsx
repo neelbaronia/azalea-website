dc0e9992a7c3bf129c425b94cfafab8fddfc13ff
Neel Baronia <nbaronia@gmail.com>
2026-09-08 10:53:53 -0700

    Link restored Tally's Corner demo from homepage footer

--- website/src/components/SiteFooter.tsx
+++ website/src/components/SiteFooter.tsx
@@ -51,6 +51,12 @@ export default function SiteFooter() {
             Audio Samples
           </a>
           <a
+            href="/demo/tallys-corner"
+            className="hover:text-white transition-colors"
+          >
+            Tally&apos;s Corner Demo
+          </a>
+          <a
             href="/translations"
             className="hover:text-white transition-colors"
           >
